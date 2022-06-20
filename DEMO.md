f65c40d8c55290280088b1823071c37ffc04100e
ymingxie <ymxyimingxie@gmail.com>
2022-06-20 00:30:22 -0400

    update demo

---
+++ DEMO.md
@@ -0,0 +1,68 @@
+
+# Run Demo with Custom ARKit Data 
+### (modified from [NeuralRecon/Demo.md](https://github.com/zju3dv/NeuralRecon/blob/master/DEMO.md))
+In this tutorial we introduce the real-time demo of NeuralRecon running with self-captured ARKit data. If you don't want to take the effort capturing your own data, you can download the [example data](https://drive.google.com/file/d/1FKccOUCW2T_rV81VhqVeqeo-dec8ooNW/view?usp=sharing) and skip step 1.
+
+To capture data and run this demo, an Apple device (iPhone or iPad) with ARKit support is required. 
+Generally speaking, devices released after 2017 (e.g. iPhone 7 and later generations) are all supported. 
+You can search for 'arkit' on [this page](https://developer.apple.com/library/archive/documentation/DeviceInformation/Reference/iOSDeviceCompatibility/DeviceCompatibilityMatrix/DeviceCompatibilityMatrix.html) to find out.
+You will also need a Mac computer to compile the data capture app and a GPU-enabled machine (GPU memory > 3GB) to run PlanarRecon.
+
+## Step 1: Capture video data with camera poses from ARKit
+For now we use [ios_logger](https://github.com/Varvrar/ios_logger) as the capture app, and you will have to compile it yourself.
+We are making an attempt to release a new capture app that is available to download from the App Store. 
+
+
+### Compile ios_logger
+
+1. Download and install [Xcode](https://apps.apple.com/us/app/xcode/id497799835?mt=12).
+2. `git clone https://github.com/Varvrar/ios_logger`
+3. Follow [this tutorial](https://ioscodesigning.com/generating-code-signing-files) to generate [a certificate](https://ioscodesigning.com/generating-code-signing-files/#generate-a-code-signing-certificate-using-xcode) and [a provisioning profile](https://ioscodesigning.com/generating-code-signing-files/#generate-a-provisioning-profile-with-xcode). (Don't be scared if you find this tutorial is very long and complex😉, it's actually quite simple with Xcode automatically handling most of the work.)
+4. Follow the [README](https://github.com/Varvrar/ios_logger#build-and-run) of ios_logger to run it on your device.
+
+### Capture the data
+You can follow [these steps](https://github.com/Varvrar/ios_logger#collect-datasets) to capture the data. 
+A clean indoor environment is prefered since it's closer to the training dataset ScanNet.
+Be sure to move around your device frequently during capture to get more views with covisibility on the same place.
+
+## Step 2: Run the demo
+After [retrieving the captured data](https://github.com/Varvrar/ios_logger#get-saved-datasets) and transfer it to a GPU-enabled machine, you are good to proceed. Notice that it's a good idea to start with the [example data](https://drive.google.com/file/d/1FKccOUCW2T_rV81VhqVeqeo-dec8ooNW/view?usp=sharing) to make sure the environment for PlanarRecon is correctly configured.
+
+1. Change the data path in [demo.yaml](config/demo.yaml).
+2. Run NeuralRecon demo: 
+`
+python demo.py --cfg ./config/demo.yaml
+`
+
+The reconstructed mesh will be available under `results/scene_demo_release_68`. 
+You can open the ply file with [MeshLab](https://www.meshlab.net/).
+
+## Illustrations to the processed data format from ios_logger
+For those who are interested to reuse the captured data for other projects:
+
+The directory structure:
+```
+DATAROOT
+└───fragments.pkl
+└───images
+│   └───0.jpg
+│   └───1.jpg
+│   |   ...
+```
+The structure of `fragments.pkl`:
+```
+[
+{'scene': scene_name: [str],
+'fragment_id': fragment id: [int],
+'image_ids': image id: [int],
+'extrinsics': poses: [matrix: 4X4],
+'intrinsics': intrinsics: [matrix: 3X3]
+}
+...
+]
+```
+
+[Here](tools/process_arkit_data.py) is the code for generating `fragments.pkl`.
+```bash
+python tools/process_arkit_data.py
+```
---
+++ tools/process_arkit_data.py
@@ -0,0 +1,118 @@
+# This file is derived from [NeuralRecon](https://github.com/zju3dv/NeuralRecon).
+# Originating Author: Yiming Xie, Jiaming Sun
+
+# Original header:
+# Copyright SenseTime. All Rights Reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import pickle
+from tqdm import tqdm
+import sys
+sys.path.append('.')
+
+from tools.kp_reproject import *
+from tools.sync_poses import *
+
+# params
+project_path = './data/demo/2022-05-09T17-21-06'
+
+def process_data(data_path, data_source='ARKit', window_size=9, min_angle=15, min_distance=0.1, ori_size=(1920, 1440), size=(640, 480)):
+    # save image
+    print('Extract images from video...')
+    video_path = os.path.join(data_path, 'Frames.m4v')
+    image_path = os.path.join(data_path, 'images')
+    if not os.path.exists(image_path):
+        os.mkdir(image_path)
+    extract_frames(video_path, out_folder=image_path, size=size)
+
+    # load intrin and extrin
+    print('Load intrinsics and extrinsics')
+    sync_intrinsics_and_poses(os.path.join(data_path, 'Frames.txt'), os.path.join(data_path, 'ARposes.txt'),
+                            os.path.join(data_path, 'SyncedPoses.txt'))
+
+    path_dict = path_parser(data_path, data_source=data_source)
+    cam_intrinsic_dict = load_camera_intrinsic(
+        path_dict['cam_intrinsic'], data_source=data_source)
+
+    for k, v in tqdm(cam_intrinsic_dict.items(), desc='Processing camera intrinsics...'):
+        cam_intrinsic_dict[k]['K'][0, :] /= (ori_size[0] / size[0])
+        cam_intrinsic_dict[k]['K'][1, :] /= (ori_size[1] / size[1])
+    cam_pose_dict = load_camera_pose(
+        path_dict['camera_pose'], data_source=data_source)
+
+    # save_intrinsics_extrinsics
+    if not os.path.exists(os.path.join(data_path, 'poses')):
+        os.mkdir(os.path.join(data_path, 'poses'))
+    for k, v in tqdm(cam_pose_dict.items(), desc='Saving camera extrinsics...'):
+        np.savetxt(os.path.join(data_path, 'poses', '{}.txt'.format(k)), v, delimiter=' ')
+
+    if not os.path.exists(os.path.join(data_path, 'intrinsics')):
+        os.mkdir(os.path.join(data_path, 'intrinsics'))
+    for k, v in tqdm(cam_intrinsic_dict.items(), desc='Saving camera intrinsics...'):
+        np.savetxt(os.path.join(data_path, 'intrinsics', '{}.txt'.format(k)), v['K'], delimiter=' ')
+
+    # generate fragment
+    fragments = []
+
+    all_ids = []
+    ids = []
+    count = 0
+    last_pose = None
+
+    for id in tqdm(cam_intrinsic_dict.keys(), desc='Keyframes selection...'):
+        cam_intrinsic = cam_intrinsic_dict[id]
+        cam_pose = cam_pose_dict[id]
+
+        if count == 0:
+            ids.append(id)
+            last_pose = cam_pose
+            count += 1
+        else:
+            angle = np.arccos(
+                ((np.linalg.inv(cam_pose[:3, :3]) @ last_pose[:3, :3] @ np.array([0, 0, 1]).T) * np.array(
+                    [0, 0, 1])).sum())
+            dis = np.linalg.norm(cam_pose[:3, 3] - last_pose[:3, 3])
+            if angle > (min_angle / 180) * np.pi or dis > min_distance:
+                ids.append(id)
+                last_pose = cam_pose
+                # Compute camera view frustum and extend convex hull
+                count += 1
+                if count == window_size:
+                    all_ids.append(ids)
+                    ids = []
+                    count = 0
+
+    # save fragments
+    for i, ids in enumerate(tqdm(all_ids, desc='Saving fragments file...')):
+        poses = []
+        intrinsics = []
+        for id in ids:
+            # Moving down the X-Y plane in the ARKit coordinate to meet the training settings in ScanNet.
+            cam_pose_dict[id][2, 3] += 1.5
+            poses.append(cam_pose_dict[id])
+            intrinsics.append(cam_intrinsic_dict[id]['K'])
+        fragments.append({
+            'scene': data_path.split('/')[-1],
+            'fragment_id': i,
+            'image_ids': ids,
+            'extrinsics': poses,
+            'intrinsics': intrinsics
+        })
+
+    with open(os.path.join(data_path, 'fragments.pkl'), 'wb') as f:
+        pickle.dump(fragments, f)
+
+if __name__ == '__main__':
+    process_data(project_path)
